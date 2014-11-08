185e9706a5ac0348f708245f5888e44163c540ca
NateMcD <ntmcdorman@gmail.com>
2014-11-08 15:20:02 -0800

    DST's

---
+++ Game/Assets/DST_Source.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DST_Source : MonoBehaviour {
+
+    public int Width, Length, Height;
+    private DST masterDST = null;
+    private bool destroyed = false;
+
+	
+	void Start () {
+        masterDST = FindObjectOfType<DST>();
+	}
+
+    public void BeginDestructionSequence()
+    {
+        if (masterDST != null)
+        {
+            masterDST.MoveTo(transform.position, Width, Length, Height, transform);
+        }
+    }
+	
+	
+	void Update () {
+        if(!destroyed)
+        {
+            if (Input.GetKey("space"))
+            {
+                BeginDestructionSequence();
+                destroyed = true;
+            }
+        }
+	    // Check for collisions
+        // Fire BeginDestructionSequence() when desired
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name.Contains("Player"))
+            BeginDestructionSequence();
+    }
+}
